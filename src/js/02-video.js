98b51da1f8e78bb32f206b5db00ffeb07aa6d841
EgorKrav <123078885+EgorKrav@users.noreply.github.com>
2023-03-30 19:19:29 +0300

    js8-hw

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,20 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('#vimeo-player');
+const player = new Player(iframe);
+
+player.on(
+  'timeupdate',
+  throttle(() => {
+    const currentTime = player.getCurrentTime();
+    localStorage.setItem('videoplayer-current-time', currentTime);
+  }, 1000)
+);
+
+document.addEventListener('DOMContentLoaded', () => {
+  const currentTime = localStorage.getItem('videoplayer-current-time');
+  if (currentTime) {
+    player.setCurrentTime(currentTime);
+  }
+});
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,40 @@
+const form = document.querySelector('.feedback-form');
+const emailInput = form.querySelector('input[name="email"]');
+const messageInput = form.querySelector('textarea[name="message"]');
 
+const saveFormState = _.throttle(() => {
+  const formState = {
+    email: emailInput.value,
+    message: messageInput.value,
+  };
+  localStorage.setItem('feedback-form-state', JSON.stringify(formState));
+}, 500);
+
+const loadFormState = () => {
+  const formState = JSON.parse(
+    localStorage.getItem('feedback-form-state') || '{}'
+  );
+  emailInput.value = formState.email || '';
+  messageInput.value = formState.message || '';
+};
+
+const clearFormState = () => {
+  localStorage.removeItem('feedback-form-state');
+  emailInput.value = '';
+  messageInput.value = '';
+};
+
+emailInput.addEventListener('input', saveFormState);
+messageInput.addEventListener('input', saveFormState);
+
+window.addEventListener('load', loadFormState);
+
+form.addEventListener('submit', event => {
+  event.preventDefault();
+  const formState = {
+    email: emailInput.value,
+    message: messageInput.value,
+  };
+  console.log(formState);
+  clearFormState();
+});
